39234eb0bfff51316cd938bcdf3ee1a237f8c784
ChristianNomena <christiangrzk@gmail.com>
2023-03-17 23:10:57 +0300

    Integrate providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:blank_photos/commons/theme.dart';
+import 'package:blank_photos/features/splash_screen/screens/splash_screen_page.dart';
+import 'package:blank_photos/providers/loading_provider.dart';
+import 'package:blank_photos/providers/photos_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Photos()),
+        ChangeNotifierProvider(create: (_) => Loading()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,25 +23,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Blank Photos',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: ColorsTheme.primary,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: ColorsTheme.primary,
+          centerTitle: true,
+        ),
       ),
-      home: const HomePage(),
+      home: const SplashScreenPage(),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }

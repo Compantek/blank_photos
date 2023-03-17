a732fe9c2344d992dcd9540f085784925d050ff2
ChristianNomena <christiangrzk@gmail.com>
2023-03-17 23:10:32 +0300

    Add constants

---
+++ lib/commons/constants.dart
@@ -0,0 +1,17 @@
+import 'package:blank_photos/commons/theme.dart';
+import 'package:flutter/material.dart';
+
+const kLightNormalTitleTextStyle = TextStyle(
+  color: ColorsTheme.light,
+  fontSize: SizesTheme.lg,
+);
+
+const kLightBigTitleTextStyle = TextStyle(
+  color: ColorsTheme.light,
+  fontSize: SizesTheme.xl,
+  fontWeight: FontWeight.bold,
+);
+
+const kSpacing = SizedBox(
+  height: SizesTheme.sm,
+);

04f983f64798be0be7658203679ea28fd5439764
ChristianNomena <christiangrzk@gmail.com>
2023-03-17 23:07:56 +0300

    Add theme

---
+++ lib/commons/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SizesTheme {
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+  static const double xxl = 64.0;
+  static const double xxxl = 128.0;
+}
+
+class ColorsTheme {
+  static const Color primary = Color(0xFF6575EB);
+  static const Color light = Color(0xFFEFEFEF);
+  static Color success = Colors.green.shade600;
+  static const Color danger = Color(0xFFCB4D49);
+}
